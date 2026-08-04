adf3e5fc45e0f8123c55db47440b75c1d5e3670a
Nina <23194405+ninarao@users.noreply.github.com>
2026-08-04 14:32:34 -0400

    Add files via upload

--- whispervtt.py
+++ whispervtt.py
@@ -12,12 +12,12 @@ from whisper.utils import get_writer
 from datetime import date
 import pandas as pd
 
-sys.argv = [
-    'whispervtt.py',
-    '/Users/nraogra/Desktop/Captioning/whisperdemo/vkttt_7min/data',
-    '-t',
-    '-o'
-    ]
+# sys.argv = [
+#     'whispervtt.py',
+#     '/Users/nraogra/Desktop/Captioning/whisperdemo/vkttt_7min/data',
+#     '-t',
+#     '-o'
+#     ]
 
 def valid_directory(path_string):
     if not os.path.isdir(path_string):
